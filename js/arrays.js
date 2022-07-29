6598ab76909b54dbed3ef5dfd047105b04774cc7
edolinares <edolinares@gmail.com>
2022-07-29 16:20:50 -0600

    entrega3

--- js/arrays.js
+++ js/arrays.js
@@ -1,18 +1,16 @@
-
-let names = [];
-let ages = [];
+let database = [];
 function getData() {
     const name = document.getElementById('name').value;
     const age = document.getElementById('age').valueAsNumber;
     const error = document.getElementById('error');
     error.innerHTML = (!name || !age) ? 'Name & Age are required.' : '';
     if (name && age) {
-        names.push(name);
-        ages.push(age);
-        console.log(names.length);
+        database.push({name,age});
+        console.log(database.length);
     }
     document.getElementById('myform').reset();
     document.getElementById('name').focus();
+    $("#tableArray").empty();
 }
 
 function publish(){
@@ -20,12 +18,10 @@ function publish(){
 }
 
 function show(){
-    for (let i = 0; i < names.length; i++) {
-        console.log("Name: "+names[i]+" Age: "+ages[i]);
+    for (let i = 0; i < database.length; i++) {
+        console.log("Name: "+database[i]);
     }
-    const average = ages.reduce((a, b) => a + b, 0) / ages.length;
-    console.log("Average of ages: "+average);
-    /* names.forEach(element => console.log(element)); */
+    generatetable();
 }
 
 var input = document.getElementById("age");
@@ -35,3 +31,42 @@ input.addEventListener("keypress", function(event) {
     getData();
   }
 });
+
+function generatetable(){
+    average();
+    const avg = document.getElementById('average');
+    avg.innerHTML = 'The average Age is: '+average()+'  ';
+    let myTable = document.querySelector('#table');
+    let headers = ['Name', 'Age'];
+    let table = document.createElement('table');
+    let headerRow = document.createElement('tr');
+    headers.forEach(headerText => {
+        let header = document.createElement('th');
+        let textNode = document.createTextNode(headerText);
+        header.appendChild(textNode);
+        headerRow.appendChild(header);
+    });
+    table.appendChild(headerRow);
+    database.forEach(emp => {
+        let row = document.createElement('tr');
+        Object.values(emp).forEach(text => {
+            let cell = document.createElement('td');
+            let textNode = document.createTextNode(text);
+            cell.appendChild(textNode);
+            row.appendChild(cell);
+        })
+        table.appendChild(row);
+    });
+    myTable.appendChild(table);
+}
+let avg = 0;
+
+function average(){
+    avg = 0;
+    for (let i = 0; i < database.length; i++) {
+        avg = avg + database[i].age;
+    }
+    avg = avg / database.length;
+    console.log("Average of ages: "+avg);
+    return avg;
+}
