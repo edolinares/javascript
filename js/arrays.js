a151a7ff7e67edf7cb3beb3b3ed576dbc254ecf1
edolinares <edolinares@gmail.com>
2022-08-01 14:55:48 -0600

    entrega5

--- js/arrays.js
+++ js/arrays.js
@@ -2,18 +2,27 @@ let database = [];
 function getData() {
     const name = document.getElementById('name').value;
     const age = document.getElementById('age').valueAsNumber;
+    let gender = "x"
+    var genders = document.getElementsByName("gender");
+    for (var i = 0, length = genders.length; i < length; i++) {
+     if (genders[i].checked) {
+        gender = genders[i].value;
+       break;
+     }
+   }
     const error = document.getElementById('error');
-    error.innerHTML = (!name || !age) ? 'Name & Age are required.' : '';
+    error.innerHTML = (!name || !age) ? 'Please fill all the form.' : '';
     if (name && age) {
-        database.push({name,age});
+        database.push({name,age,gender});
         console.log(database.length);
     }
-    document.getElementById('myform').reset();
-    document.getElementById('name').focus();
+   document.getElementById('myform').reset();
+   document.getElementById('name').focus();
+   generetotals();
 }
 
 function publish(){
-    alert('Se crearon '+names.length+' registros');
+    alert('Se crearon '+database.length+' registros');
 }
 
 function show(){
@@ -21,6 +30,7 @@ function show(){
         console.log("Name: "+database[i]);
     }
     generatetable();
+    generategraph();
 }
 
 var input = document.getElementById("age");
@@ -36,7 +46,7 @@ function generatetable(){
     const avg = document.getElementById('average');
     avg.innerHTML = 'The average Age is: '+average()+'  ';
     let myTable = document.querySelector('#table');
-    let headers = ['Name', 'Age'];
+    let headers = ['Name', 'Age','Sex'];
     let table = document.createElement('table');
     let headerRow = document.createElement('tr');
     headers.forEach(headerText => {
@@ -66,6 +76,52 @@ function average(){
         avg = avg + database[i].age;
     }
     avg = avg / database.length;
+    avg = avg.toFixed(2);
     console.log("Average of ages: "+avg);
     return avg;
+}
+
+let m = 0;
+let f = 0;
+
+function generetotals(){
+    m = 0;
+    f = 0;
+    for (let i = 0; i < database.length; i++) {
+        if(database[i].gender == "man"){ 
+            m += 1;
+        } 
+        else {
+            f += 1;
+        }
+    }
+    console.log("male: "+m+" female: "+f);
+}
+
+function generategraph(){
+    const labels = [
+        'Man',
+        'Woman'
+      ];
+    
+      const data = {
+        labels: labels,
+        datasets: [{
+          label: 'Quantity',
+          backgroundColor: ['rgb(46, 70, 171)','rgb(255, 99, 132)'],
+          borderColor: 'rgb(255, 255, 255)',
+          data: [m, f],
+        }]
+      };
+    
+      const config = {
+        type: 'doughnut',
+        data: data,
+        options: {}
+      };
+    
+      const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
 }
